b37a29800ab07ca68a9da0f7a173dffc82bdb483
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-01-07 15:48:50 -0600

    for some reason this was lost

---
+++ src/components/SliderData.js
@@ -0,0 +1,14 @@
+export const SliderData = [
+    {
+        image: 'https://unsplash.com/photos/e4et737H4ow/download?force=true&w=1920'
+    },
+    {
+        image: 'https://unsplash.com/photos/ZBM9h6Es6IA/download?force=true&w=1920'
+    },
+    {
+        image: 'https://unsplash.com/photos/enGbV7gcoH4/download?force=true&w=1920'
+    },
+    {
+        image: 'https://unsplash.com/photos/Hs-Tt4fBX3M/download?force=true&w=1920'
+    }
+]
